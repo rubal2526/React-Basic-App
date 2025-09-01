e24088ea8a465236691b370003d5daafd596f083
rubal2526 <pig121512141@gmail.com>
2025-09-01 13:18:12 +0900

    MyComfunc update

--- src/components/MyComponentsFunc.jsx
+++ src/components/MyComponentsFunc.jsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 
 const MyComponentFunc = ({ name, children }) => {
     //useState() 함수사용
-    const [value,setValue] = useState(0);
-    const [inputs, setInpusts] = useState({
-        message: '', 
+    const [value, setValue] = useState(0);
+    const [inputs, setInputs] = useState({
+        message: '',
         username: ''
     });
 
-    const { message, username} = inputs;
+    const { message, username } = inputs;
+
+    //event handlers
+    const handleChange = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
 
     return (
         <div>
@@ -16,8 +24,14 @@ const MyComponentFunc = ({ name, children }) => {
             <h3>Hello! {name}</h3>
             {children}
             <p>상태변수 value = {value}</p>
-            <button onClick={()=>(setValue(value + 1))}>증가</button>
-            <button onClick={()=>(setValue(value - 1))}>감소</button>
+            <button onClick={() => (setValue(value + 1))}>증가</button>
+            <button onClick={() => (setValue(value - 1))}>감소</button>
+            <br />
+            <p>상태변수 message = {message}</p>
+            <input name="message" value={message} onChange={handleChange} />
+            <p>상태변수 username = {username}</p>
+            <input name="username" value={username} onChange={handleChange} />
+
         </div>
     );
 };
