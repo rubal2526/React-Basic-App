56912273672199daad5271e8e8b7ac5c5dd52498
rubal2526 <pig121512141@gmail.com>
2025-09-01 10:59:27 +0900

    MyComfunc update

--- src/components/MyComponentsFunc.jsx
+++ src/components/MyComponentsFunc.jsx
@@ -1,13 +1,19 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 
-const MyComponentsFunc = ({ name, children }) => {
+const MyComponentFunc = ({ name, children }) => {
+    //useState() 함수사용
+    const [value,setValue] = useState(0);
+
     return (
         <div>
             <h2>함수형 컴포넌트</h2>
             <h3>Hello! {name}</h3>
             {children}
+            <p>상태변수 value = {value}</p>
+            <button onClick={()=>(setValue(value + 1))}>증가</button>
+            <button onClick={()=>(setValue(value - 1))}>감소</button>
         </div>
     );
 };
 
-export default MyComponentsFunc;
+export default MyComponentFunc;
