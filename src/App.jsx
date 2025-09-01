017c9b52461c62007839c719debfb98d1004cbe4
rubal2526 <pig121512141@gmail.com>
2025-09-01 10:20:54 +0900

    func add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import MyComponents from "./components/MyComponents";
+import MyComponentsFunc from "./components/MyComponentsFunc";
 
 const App = () => {
   return (
     <>
       <MyComponents name="ReactJS" />
       <MyComponents age={100} />
+
+      <hr/>
+      <MyComponentsFunc name="함수형">
+        <p>함수형 컴포넌트의 하위 엘리먼트</p>
+      </MyComponentsFunc>
     </>
   );
 };
